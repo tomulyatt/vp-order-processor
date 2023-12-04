e4eddf297eeb2154caecc86e9e902e882db88c26
Tom Ulyatt <tomulyatt@gmail.com>
2023-12-04 08:55:38 +0000

    Update CreateOrderRequest record to use middleware validation. Allows for early capture of errors based on pre-configured data attributes

--- VPOrderProcessor.Contracts/Orders/CreateOrderRequest.cs
+++ VPOrderProcessor.Contracts/Orders/CreateOrderRequest.cs
@@ -1,18 +1,102 @@
-﻿namespace VPOrderProcessor.Contracts.Orders
-{
-    public record CreateOrderRequest(
-        string ExternalOrderId,
-        DateTime OrderCreatedDateTime,
-        OrderPrice OrderTotal,
-        Customer Customer,
-        List<Product> Products,
-        List<CreateOrderPayment>? Payments);
+﻿using System.ComponentModel.DataAnnotations;
 
-    public record OrderPrice(long Amount, string CurrencyId);
-    public record Customer(string Forename, string Surname, string Email, string TelephoneNumber, Address CustomerAddress);
-    public record Address(string Street1, string? Street2, string? Street3, string PostCode, string City, string Country);
-    public record Product(string ProductName, double ProductQuantity, string ProductCode, string ProductSKU, 
-        ProductPrice UnitPrice, ProductPrice SellPrice, ProductPrice UnitTax, ProductPrice ItemTax);
-    public record ProductPrice(long Amount, string CurrencyId);
-    public record CreateOrderPayment(long Amount, string MethodOfPaymentId);
+namespace VPOrderProcessor.Contracts.Orders
+{
+    public record CreateOrderRequest
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string ExternalOrderId { get; init; }
+
+        [Required]
+        public DateTime OrderCreatedDateTime { get; init; }
+
+        [Required]
+        public OrderPrice OrderTotal { get; init; }
+
+        [Required]
+        public Customer Customer { get; init; }
+
+        [Required]
+        public List<Product> Products { get; init; }
+        public List<CreateOrderPayment>? Payments { get; init; }
+    }
+
+    public record OrderPrice 
+    {
+        [Range(0, int.MaxValue)]
+        public long Amount { get; init; }
+
+        [Required(AllowEmptyStrings =false)]
+        public string CurrencyId { get; init; }
+    }
+    public record Customer
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Forename { get; init; }
+
+
+        [Required(AllowEmptyStrings = false)]
+        public string Surname { get; init; }
+
+
+        [Required(AllowEmptyStrings = false)]
+        public string Email { get; init; }
+
+
+        [Required(AllowEmptyStrings = false)]
+        public string TelephoneNumber { get; init; }
+        public Address CustomerAddress { get; init; }
+    }
+    public record Address
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Street1 { get; init; }
+        public string? Street2 { get; init; }
+        public string? Street3 { get; init; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string PostCode { get; init; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string City { get; init; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Country { get; init; }    
+    }
+    public record Product
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string ProductName { get; init; }
+
+        [Range(0, double.MaxValue)]
+        public double ProductQuantity { get; init; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string ProductCode { get; init; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string ProductSKU { get; init; }
+        public ProductPrice UnitPrice { get; init; }
+        public ProductPrice SellPrice { get; init; }
+        public ProductPrice UnitTax { get; init; }
+        public ProductPrice ItemTax { get; init; }
+    }
+    
+    
+    public record ProductPrice
+    {
+        [Range(0, int.MaxValue)]
+        public long Amount { get; init; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string CurrencyId { get; init; }
+    }
+    public record CreateOrderPayment
+    {
+        [Range(0, int.MaxValue)]
+        public long Amount { get; init; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string MethodOfPaymentId { get; init; }
+    }
 }
